caf58db107f866f06a0ec1cf367b3696d8baf15a
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2024-10-11 23:40:31 -0300

    commit 2
    meu segundo commit

--- dev_api/app.py
+++ dev_api/app.py
@@ -3,6 +3,7 @@ import json
 
 app = Flask(__name__)
 
+#listinha com os desenvolvedores
 desenvolvedores = [
     {
         "nome": "Rafael",
@@ -15,18 +16,38 @@ desenvolvedores = [
 
 ]
 
-
-@app.route('/dev/<int:id>/', methods=["GET","PUT"])
+#Devolve, altera e remove um desenvolvedor pelo ID
+@app.route('/dev/<int:id>/', methods=["GET","PUT","DELETE"])
 def desenvolvedor(id):
 
     if request.method == "GET":
-        desenvolvedor = desenvolvedores[id]
-        return jsonify(desenvolvedor)
+        try:
+            response = desenvolvedores[id]
+        
+        except IndexError:
+            mensagem = f"Desenvolvedor de ID: {id} não encontrado"        
+            response = {"Status": "FALHA", "Mensagem": mensagem}
+
+        except Exception:
+            mensagem = "Erro desconhecido. Procure o administrador da API."
+            response = {"Status": "FALHA", "Mensagem": mensagem}
+        
+        return jsonify(response)    
+    
+    
     elif request.method == "PUT":
         
         dados = json.loads(request.data)
         desenvolvedores[id] = dados
+        return jsonify(dados)
+    
 
+    elif request.method == "DELETE":
+        desenvolvedores.pop(id)
+        return jsonify({'status': "sucesso", "mensagem": "registro excluído"})
+
+
+def lista_desenvolvedores():
 
 if __name__ == "__main__":
     app.run(debug=True)
